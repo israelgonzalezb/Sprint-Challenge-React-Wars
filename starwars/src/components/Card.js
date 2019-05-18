66da21a42c492a0222adeaa550b04406111ecc9a
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-05-17 19:51:03 -0700

    Added birth year item to each character listed

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -3,7 +3,11 @@ import React from "react";
 function Card(props) {
   let characters = () => {
     return props.characters.map(x => {
-      return <li className="character">{x.name}</li>;
+      return (
+        <li className="character">
+          {x.name} - Born: {x.birth_year}
+        </li>
+      );
     });
   };
 
